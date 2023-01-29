65711d7fd7e25900a8426bc87bdf0c3f793a0c0b
Martin Obert <30500239+martin-obert@users.noreply.github.com>
2023-01-29 18:21:13 +0100

    Initial commit

---
+++ Assets/Scripts/Runtime/Test.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Obert.Template.Runtime
+{
+    public class Test : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
